93d5353109c4d1717b05a84a1767796d7b88f979
Faizan Ali Baig <142169392+faizanalibaig@users.noreply.github.com>
2024-06-25 22:10:42 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app=express();
 
 app.use(cors());
 
-app.post('/api/product',()=>{
+app.post('/api/product',(req, res)=>{
    res.send('working');
+})
+
+app.get('/message',(req, res)=>{
+   res.send('message');
 })
 
